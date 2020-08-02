cfd1f9bfd4f604a7ad9810e7de599623c38b6b64
Masoud Kazemi <mkazemi73@gmail.com>
2020-08-02 16:26:03 +0430

    Fix incorrect embedding bug in spaCy language class

--- tests/test_lang/test_spacy_language.py
+++ tests/test_lang/test_spacy_language.py
@@ -4,7 +4,59 @@ import numpy as np
 from spacy.vocab import Vocab
 from spacy.language import Language
 from whatlies.language import SpacyLanguage
-from whatlies.language.spacy_lang import _selected_idx_spacy
+
+
+@pytest.fixture(scope="module")
+def lang():
+    return SpacyLanguage("en_core_web_md")
+
+
+def test_basic_usage(lang):
+    queries = [
+        "yesterday is history",
+        "tomorrow is mystery",
+        "today is a gift",
+        "that's why it's called [present]",
+    ]
+    emb = lang[queries]
+    assert len(emb) == 4
+    assert emb[queries[0]].name == "yesterday is history"
+    assert emb[queries[0]].vector.shape == (300,)
+
+
+@pytest.mark.parametrize(
+    "query",
+    ["no [closing bracket", "no opening] bracket", "[more] than one [context]",],
+)
+def test_invalid_query_raises_error(lang, query):
+    with pytest.raises(ValueError, match="bracket"):
+        lang[query]
+
+
+@pytest.mark.parametrize(
+    "query, context, context_pos",
+    [
+        ("I'm going [home]", "home", (3, 4)),
+        ("you should pre-order your [washing machine]", "washing machine", (6, 8)),
+        ("today is the [best day] of my life", "best day", (3, 5)),
+        ("[yesterday] isn't mystery", "yesterday", (0, 1)),
+        (
+            "Let's have Fun without context.",
+            "Let's have Fun without context.",
+            (0, None),
+        ),
+    ],
+)
+def test_embedding_is_correct(lang, query, context, context_pos):
+    emb = lang[query]
+    assert emb.vector.shape == (300,)
+    assert emb.name == query
+
+    clean_query = query.replace("[", "").replace("]", "")
+    doc = lang.model(clean_query)
+    span = doc[context_pos[0] : context_pos[1]]
+    assert str(span) == context
+    assert np.allclose(emb.vector, span.vector)
 
 
 @pytest.fixture()
@@ -44,23 +96,10 @@ def test_lang_retreival(color_lang, string, array):
 
 def test_single_token_words(color_lang):
     # test for issue here: https://github.com/RasaHQ/whatlies/issues/5
-    assert np.sum(color_lang["red"].vector) > 0
+    assert len(color_lang["red"].vector) > 0
 
 
 def test_raise_warning(color_lang):
     print([w for w in color_lang.nlp.vocab])
     with pytest.warns(UserWarning):
         color_lang.score_similar("red", 100, prob_limit=None, lower=False)
-
-
-@pytest.mark.parametrize(
-    "triplets",
-    zip(
-        ["red", "red green", "red green [blue] purple", "red [green blue] pink"],
-        [0, 0, 2, 1],
-        [1, 2, 3, 3],
-    ),
-)
-def test_select_idx_func(triplets):
-    string, start, end = triplets
-    assert _selected_idx_spacy(string) == (start, end)

6f953469d9e2bc37c1fde09cb908a7bfb1ac5acd
Vincent <vincentwarmerdam@gmail.com>
2020-08-14 17:07:56 +0200

    pretty-import-msg

--- whatlies/error.py
+++ whatlies/error.py
@@ -1,4 +1,11 @@
 class NotInstalled:
+    """
+    This object is used for optional dependencies. If a backend is not installed we
+    replace the transformer/language with this object. This allows us to give a friendly
+    message to the user that they need to install extra dependencies as well as a link
+    to our documentation page.
+    """
+
     def __init__(self, tool, dep):
         self.tool = tool
         self.dep = dep

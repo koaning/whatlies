fd93f77916ccda2952c2726d29b528160e35b48b
vincent d warmerdam <vincentwarmerdam@gmail.com>
2020-08-14 18:28:54 +0200

    Fix #139: Conditional Imports (#141)
    * added-test-should-fail
    
    * added try/except
    
    * fix
    
    * added NotInstalled class for better communication
    
    * typo
    
    * init
    
    * github-clarity
    
    * gah! more typos!
    
    * also show pretty error msg
    
    * pretty-import-msg
    
    * small cleanup in files
    
    * remove-redundant-file

--- whatlies/transformers/__init__.py
+++ whatlies/transformers/__init__.py
@@ -3,7 +3,16 @@ from whatlies.transformers.umap import Umap
 from whatlies.transformers.noise import Noise
 from whatlies.transformers.addrandom import AddRandom
 from whatlies.transformers.tsne import Tsne
-from whatlies.transformers.opentsne import OpenTsne
-from whatlies.transformers.ivis import Ivis
+from whatlies.error import NotInstalled
+
+try:
+    from whatlies.transformers.opentsne import OpenTsne
+except ModuleNotFoundError:
+    OpenTsne = NotInstalled("OpenTsne", "opentsne")
+
+try:
+    from whatlies.transformers.ivis import Ivis
+except ModuleNotFoundError:
+    Ivis = NotInstalled("Ivis", "ivis")
 
 __all__ = ["Pca", "Umap", "Noise", "AddRandom", "Tsne", "OpenTsne", "Ivis"]

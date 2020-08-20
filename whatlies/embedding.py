95bb15393d1f0ed907685d76d174e488348aef93
vincent d warmerdam <vincentwarmerdam@gmail.com>
2020-08-20 13:38:38 +0200

    arrow (#151)

--- whatlies/embedding.py
+++ whatlies/embedding.py
@@ -177,7 +177,7 @@ class Embedding:
 
     def plot(
         self,
-        kind: str = "scatter",
+        kind: str = "arrow",
         x_axis: Union[str, "Embedding"] = None,
         y_axis: Union[str, "Embedding"] = None,
         color: str = None,

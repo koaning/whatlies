918f6b589f25e6648ffc947bb9a0ae4fa89b82a2
vincent d warmerdam <vincentwarmerdam@gmail.com>
2020-08-20 10:49:55 +0200

    Version Bump (#149)
    * version
    
    * added-docs

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ dev_packages = docs_packages + test_packages + extra_deps
 
 setup(
     name="whatlies",
-    version="0.4.6",
+    version="0.4.7",
     author="Vincent D. Warmerdam",
     packages=find_packages(exclude=["notebooks", "docs"]),
     description="Tools to help uncover `whatlies` in word embeddings.",

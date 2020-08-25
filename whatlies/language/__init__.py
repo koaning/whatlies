45b7ee597916f51225d3bb08dc1b41f047563dde
Vincent <vincentwarmerdam@gmail.com>
2020-08-25 10:44:29 +0200

    move import down

--- whatlies/language/__init__.py
+++ whatlies/language/__init__.py
@@ -1,5 +1,4 @@
 from .spacy_lang import SpacyLanguage
-from .sense2vec_lang import Sense2VecLanguage
 from .fasttext_lang import FasttextLanguage
 from .countvector_lang import CountVectorLanguage
 from .bpemblang import BytePairLanguage

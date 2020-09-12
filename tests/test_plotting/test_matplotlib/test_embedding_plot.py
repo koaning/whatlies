30ed993fcd66b1da52441ae29a148d277d5ba6e4
Masoud Kazemi <mkazemi73@gmail.com>
2020-09-12 15:56:25 +0430

    Fix #217 - Add `__init__.py` files to test directories

--- tests/test_plotting/test_matplotlib/test_embedding_plot.py
+++ tests/test_plotting/test_matplotlib/test_embedding_plot.py
@@ -4,7 +4,7 @@ import matplotlib as mpl
 import scipy.spatial.distance as scipy_distance
 
 from whatlies import Embedding, EmbeddingSet
-from common import validate_plot_general_properties
+from .common import validate_plot_general_properties
 
 
 """

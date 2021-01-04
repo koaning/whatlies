f20033f3e027ad300c5636b03a21fd2e4429a122
koaning <vincentwarmerdam@gmail.com>
2021-01-04 11:06:05 +0100

    version

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ dev_packages = docs_packages + test_packages + extra_deps
 
 setup(
     name="whatlies",
-    version="0.5.9",
+    version="0.5.10",
     author="Vincent D. Warmerdam",
     packages=find_packages(exclude=["notebooks", "docs"]),
     description="Tools to help uncover `whatlies` in word embeddings.",

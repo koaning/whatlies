d0c6a7c15316fc1fbf7f46572182b2857feef710
koaning <vincentwarmerdam@gmail.com>
2021-02-02 10:19:39 +0100

    removed-ivis-tests

--- tests/test_transformers.py
+++ tests/test_transformers.py
@@ -63,8 +63,6 @@ transformers = [
     Tsne(2, n_iter=250),
     Tsne(3, n_iter=250),
     OpenTsne(2, n_iter=2),
-    # Ivis(2, k=10, batch_size=10, epochs=10),
-    # Ivis(3, k=10, batch_size=10, epochs=10),
     Normalizer(),
     Normalizer(feature=True),
 ]
@@ -88,7 +86,6 @@ def test_transformations_new_size(transformer, extra_size):
         Noise(0.1),
         Tsne(2, n_iter=250),
         OpenTsne(2, n_iter=1),
-        # Ivis(2, k=10, batch_size=10, epochs=10),
         AddRandom(n=4),
         lambda d: d | (d["man"] - d["woman"]),
         Normalizer(),
@@ -109,7 +106,6 @@ def test_transformations_keep_props(transformer):
         AddRandom(n=4),
         Tsne(2, n_iter=250),
         OpenTsne(2, n_iter=2),
-        # Ivis(2, k=10, batch_size=10, epochs=10),
         Normalizer(),
     ],
 )

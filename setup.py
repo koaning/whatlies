c7a8857fef9bff10183671365f662a5f619f26fd
Vincent <vincentwarmerdam@gmail.com>
2022-05-27 07:48:14 +0200

    add-versions

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 
 
 base_packages = [
-    "scikit-learn>=0.24.0",
-    "altair>=4.0.1",
-    "matplotlib>=3.2.0",
+    "scikit-learn>=1.1.0",
+    "altair>=4.2.0",
+    "matplotlib>=3.5.0",
     "bpemb>=0.3.0",
     "gensim~=3.8.3",
 ]
 
 umap_packages = [
-    "umap-learn>=0.4.0",
+    "umap-learn>=0.5.0",
 ]
 
 fasttext_packages = [
@@ -21,23 +21,23 @@ fasttext_packages = [
 floret_packages = fasttext_packages + ["floret>=0.10.1"]
 
 spacy_packages = [
-    "spacy>=3.2.0",
+    "spacy>=3.3.0",
     "spacy-lookups-data>=0.3.2",
 ]
 
-s2v_packages = ["sense2vec>=1.0.2"] + spacy_packages
+s2v_packages = ["sense2vec>=2.0.0"] + spacy_packages
 
 tf_packages = [
-    "tensorflow>=2.3.0,<2.5",
-    "tensorflow-text>=2.3.0",
-    "tensorflow-hub>=0.8.0",
+    "tensorflow<2.10,>=2.9.0",
+    "tensorflow-text<2.10,>=2.9.0",
+    "tensorflow-hub>=0.12.0,<1.0",
 ]
 
 transformers_dep = [
-    "transformers>=4.3.0",
+    "transformers>=4.19.0",
 ]
 
-sentence_tfm_dep = ["sentence-transformers>=0.3.8"]
+sentence_tfm_dep = ["sentence-transformers>=2.2.0"]
 
 docs_packages = [
     "mkdocs==1.1",
@@ -49,7 +49,7 @@ docs_packages = [
 ]
 
 test_packages = [
-    "torch>=1.6.0",
+    "torch>=1.11.0",
     "flake8>=3.6.0",
     "pytest>=4.0.2",
     "black>=19.3b0",
@@ -72,7 +72,7 @@ dev_packages = docs_packages + test_packages + all_deps
 
 setup(
     name="whatlies",
-    version="0.6.5",
+    version="0.7.0",
     author="Vincent D. Warmerdam",
     packages=find_packages(exclude=["notebooks", "docs"]),
     description="Tools to help uncover `whatlies` in word embeddings.",
